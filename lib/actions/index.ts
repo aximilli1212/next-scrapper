cfef3190bc4a579bb800f0a6529c1a89a5bec3e9
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-02 08:24:40 +0000

    add utils for extracting data

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,12 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-import Product from "../models/product.model";
-import { connectToDB } from "../mongoose";
+// import Product from "../models/product.model";
+// import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
-import { generateEmailBody, sendEmail } from "../nodemailer";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
